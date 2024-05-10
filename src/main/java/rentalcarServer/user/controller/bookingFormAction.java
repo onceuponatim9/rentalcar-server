ee3ff965d35967d9d06f1ceee84de92b30bdc43f
onceuponatim9 <sjung8018a@gmail.com>
2024-05-10 18:31:47 +0900

    Update bookingFormAction.java

--- src/main/java/rentalcarServer/user/controller/bookingFormAction.java
+++ src/main/java/rentalcarServer/user/controller/bookingFormAction.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class bookingFormAction
  */
-@WebServlet("/bookingFormAction")
 public class bookingFormAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
